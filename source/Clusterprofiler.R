a1c16435855b6aefc2bad6982592336a8f1b6682
ry147 <ry147@rxd.rutgers.edu>
2018-12-06 16:05:24 -0500

    Rscript drafting

---
+++ source/Clusterprofiler.R
@@ -0,0 +1,20 @@
+################################
+#Project: UVB_UA RNA-seq project
+#Package: Clusterprofiler
+#Author: Ran Yin
+#Date: 12-6-2018
+################################
+
+#Require org.Hs.eg.db
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("org.Hs.eg.db")
+# source("https://bioconductor.org/biocLite.R")
+# biocLite("clusterProfiler")
+
+library(dplyr)
+library(tidyr)
+library(DOSE)
+library(GO.db)
+library(org.Hs.eg.db)
+library(GSEABase)
+library(clusterProfiler)
